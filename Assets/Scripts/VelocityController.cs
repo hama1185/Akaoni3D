6601369787c25caaf2c7b2527100b8186c3820ab
toneDrei79 <tone0709@icloud.com>
2019-12-05 12:54:55 +0900

    Change setting to PC debugging.

--- Assets/Scripts/VelocityController.cs
+++ Assets/Scripts/VelocityController.cs
@@ -17,18 +17,6 @@ public class VelocityController : MonoBehaviour {
     }
 
     void Update() {
-		// // // ゲームパッドによる速度入力
-
-        // float moveLR = 0.0f;
-        // float moveFB = 0.0f;
-        // moveLR = Input.GetAxis(key.moveH) * speed;
-	    // moveFB = Input.GetAxis(key.moveV) * speed;
-
-        // inputAxis_Left = new Vector3(moveLR, 0.0f, moveFB);
-        // inputAxis_Left = this.transform.rotation * inputAxis_Left;
-
-        // // //
-
         inputAxis_Left *= speed;
 
         float magnitude = Mathf.Sqrt(Mathf.Pow(inputAxis_Left.x, 2) + Mathf.Pow(inputAxis_Left.z, 2));
