8ab366ecd8ee5b43d0f5291f5e4dcaf21104470f
toneDrei79 <tone0709@icloud.com>
2020-02-01 14:42:25 +0900

    スマホ用設定を適用(修正)

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -32,28 +32,28 @@ public class CharacterMovement : MonoBehaviour {
 
 
         // // / ゲームパッドによる入力(ここから)
-        Vector3 angle = Vector3.zero;
-        float rotH = 0.0f;
-        float rotV = 0.0f;
+        // Vector3 angle = Vector3.zero;
+        // float rotH = 0.0f;
+        // float rotV = 0.0f;
 
-        rotH = Input.GetAxis(key.viewH) * sensitivity;
-	    // rotV = Input.GetAxis(key.viewV) * sensibility;
+        // rotH = Input.GetAxis(key.viewH) * sensitivity;
+	    // // rotV = Input.GetAxis(key.viewV) * sensibility;
 
-        angle = new Vector3(rotV, rotH, 0.0f);
+        // angle = new Vector3(rotV, rotH, 0.0f);
 
-        this.transform.GetChild(0).GetChild(0).GetChild(0).GetChild(0).transform.Rotate(angle);
+        // this.transform.GetChild(0).GetChild(0).GetChild(0).GetChild(0).transform.Rotate(angle);
 
-        Vector3 movement = Vector3.zero;
-		float moveLR = 0.0f;
-        float moveFB = 0.0f;
+        // Vector3 movement = Vector3.zero;
+		// float moveLR = 0.0f;
+        // float moveFB = 0.0f;
         
-        moveLR = Input.GetAxis(key.moveH) * VelocityController.speed;
-	    moveFB = Input.GetAxis(key.moveV) * VelocityController.speed;
+        // moveLR = Input.GetAxis(key.moveH) * VelocityController.speed;
+	    // moveFB = Input.GetAxis(key.moveV) * VelocityController.speed;
 
-        movement = new Vector3(moveLR, 0.0f, moveFB);
-        movement = this.transform.GetChild(0).GetChild(0).GetChild(0).GetChild(0).rotation * movement;
+        // movement = new Vector3(moveLR, 0.0f, moveFB);
+        // movement = this.transform.GetChild(0).GetChild(0).GetChild(0).GetChild(0).rotation * movement;
 
-        character.velocity = movement;
+        // character.velocity = movement;
         // // / (ここまで)
 	}
 }
