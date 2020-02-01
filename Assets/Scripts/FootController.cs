4f168d5596daf1bc04a93ae2d5bbdd6d14a7fd1f
hama1185 <hamataku7332@softbank.ne.jp>
2020-02-01 13:25:41 +0900

    仕様変更:stepのとる値を70から100~1100

--- Assets/Scripts/FootController.cs
+++ Assets/Scripts/FootController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class FootController : MonoBehaviour
 {
+    public int initial = 600;
     // Start is called before the first frame update
     void Start(){
         StartCoroutine(Disappearing());
@@ -12,7 +13,7 @@ public class FootController : MonoBehaviour
     // Update is called once per frame
     IEnumerator Disappearing()//コルーチン
     {
-        int step = 120 + (int)(PlayerStatus.mind - EnemyStatus.relaxed);
+        int step = initial + (int)(PlayerStatus.mind - EnemyStatus.relaxed)*5;
         for (int i = 0; i < step; i++)
         {
             GetComponent<MeshRenderer> ().material.color = new Color (1, 1, 1, 1 - 1.0f * i / step);
